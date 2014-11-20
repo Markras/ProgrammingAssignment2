896a12cbaf22feda9290e7913381c979b8e50de0
Markras <marcin.krasnodebski1@gmail.com>
2014-11-20 23:23:49 +0100

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,34 @@
 ## Put comments here that give an overall description of what your
 ## functions do
 
-## Write a short comment describing this function
+## Well, this function does exactly what the function "makeVector" already provided by coursera,
+#but instead of creating a variable of a vector with a cached mean, it creates a variaable containing a matrix with a cached inverse.
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  m <- NULL
+  set <- function(y) {
+    x <<- y
+    m <<- NULL
+  }
+  get <- function() x
+  setinverse <- function(solve) m <<- solve
+  getinverse <- function() m
+  list(set = set, get = get,
+       setinverse = setinverse,
+       getinverse = getinverse)
 }
 
 
-## Write a short comment describing this function
+## This function allows you to manipulate variables created using makeCacheMatrix. It either calculates the inverse of the matrix, or if the inverse is already cached in a variable, it retrieves it.
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  m <- x$getinverse()
+  if(!is.null(m)) {
+    message("getting cached data")
+    return(m)
+  }
+  data <- x$get()
+  m <- solve(data, ...)
+  x$setinverse(m)
+  m
 }
